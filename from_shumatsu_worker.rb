258d34310ef84085be8e8a0f2f3937faa814bca1
kazuyuki ikeda <gankai1104@gmail.com>
2018-04-09 09:15:07 +0900

    Fix rubocop

--- from_shumatsu_worker.rb
+++ from_shumatsu_worker.rb
@@ -1,8 +1,9 @@
 require './base_crawler'
 require 'pry-byebug'
 
+# shuuumatu-worker.jp crawler
 class FromShumatsuWorker < BaseCralwer
-  def call
+  def initialize
     @current_max_page_number = 1
     robotex = ::Robotex.new
     return unless robotex.allowed?(base_uri)
@@ -12,33 +13,24 @@ class FromShumatsuWorker < BaseCralwer
       http_open_timeout: 5
     )
     @bucket = @s3.bucket('grabat-crawler')
+    super
+  end
 
+  def call
     last_page_number
-
-    1.upto(@current_max_page_number) do |i|
-      doc = access_site("/list/page/#{i}")
-      items = doc.xpath("//div[@class='m-worklist__caption']/a")
-      items.each do |item|
-        detail_page = access_site('/' + item['href'].match(/\d+/)[0])
-        file = @bucket.object(item['href'].match(/\d+/)[0] + '.html')
-        file.put(body: detail_page.document.to_s)
-      end
-    end
+    crawl
   end
 
   def last_page_number
     loop do
-      begin
-        doc = access_site("/list/page/#{@current_max_page_number}")
-        pagenates = doc.xpath("//div[@class='yutopro_pagenavi']/a")
-        current_max_page = pagenates.map do |page|
-          page['href'] =~ /page\/(\d+)\z/
-        end.compact.max || 1
-        break if current_max_page == @current_max_page_number || @current_max_page_number == 1
-        @current_max_page_number = current_max_page
-      rescue StandardError
-        break
-      end
+      puts @current_max_page_number
+      doc = access_site("/list/page/#{@current_max_page_number}")
+      pagenates = doc.xpath("//div[@class='yutopro_pagenavi']/a")
+      current_max_page = pagenates.map do |page|
+        page['href'] =~ %r{page\/(\d+)\z}
+      end.compact.max || 1
+      break if [@current_max_page_number, 1].include?(current_max_page)
+      @current_max_page_number = current_max_page
     end
   end
 
@@ -50,6 +42,27 @@ class FromShumatsuWorker < BaseCralwer
       f.read
     end
     Nokogiri::HTML.parse(html, nil, @charset)
+  end
+
+  def crawl
+    1.upto(@current_max_page_number) do |i|
+      doc = access_site("/list/page/#{i}")
+      items = doc.xpath("//div[@class='m-worklist__caption']/a")
+      save(items)
+    end
+  end
+
+  def save(items)
+    items.each do |item|
+      detail_page = access_site('/' + item['href'].match(/\d+/)[0])
+      upload_to_s3(detail_page)
+    end
+  end
+
+  def upload_to_s3(detail_page)
+    file = @bucket.object(Time.now.strftime('%Y%m%d') + item['href']
+      .match(/\d+/)[0] + '.html')
+    file.put(body: detail_page.document.to_s)
   end
 end
 
